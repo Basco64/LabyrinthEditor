cbc44208d89578d15679a7db427ab8f9cdee34e9
Basco64 <martinez.xabi@laposte.net>
2023-09-22 22:03:32 +0200

    first commit

--- main.js
+++ main.js
@@ -1,229 +1,97 @@
 const myGame = document.querySelector("#game");
 const alert = document.querySelector(".alert");
-const checkIA = document.querySelector("#IA");
 
-let sizeIMG = 100;
+let line1 = [0, 0, 0];
+let line2 = [0, 0, 0];
+let line3 = [0, 0, 0];
+let arrGame = [line1, line2, line3];
 
-let totalLine = 4;
-let totalColumn = 4;
+displayGrid();
 
-let playerPosition = [0, 0];
-let currentLevel = 0;
-let arrGame = null;
-
-let gameOver = false;
-let isIAOn = false;
-
-nextLevel();
-
-function startIA() {
-  checkIA.setAttribute("disabled", "disabled");
-  isIAOn = true;
-  IA.startIA();
-}
-
-function createCell(image) {
-  let cell = {
-    img: image,
-    left: getLeft(image),
-    top: getTop(image),
-    right: getRight(image),
-    bottom: getBot(image),
-  };
-  return cell;
-}
-
-function getLeft(image) {
-  if (
-    image === 0 ||
-    image === 1 ||
-    image === 2 ||
-    image === 3 ||
-    image === 6 ||
-    image === 7 ||
-    image === 8 ||
-    image === 12
-  )
-    return true;
-  return false;
-}
-
-function getTop(image) {
-  if (
-    image === 0 ||
-    image === 2 ||
-    image === 3 ||
-    image === 4 ||
-    image === 5 ||
-    image === 8 ||
-    image === 9 ||
-    image === 14
-  )
-    return true;
-  return false;
-}
-
-function getRight(image) {
-  if (
-    image === 0 ||
-    image === 1 ||
-    image === 3 ||
-    image === 4 ||
-    image === 6 ||
-    image === 9 ||
-    image === 10 ||
-    image === 13
-  )
-    return true;
-  return false;
-}
-
-function getBot(image) {
-  if (
-    image === 0 ||
-    image === 1 ||
-    image === 2 ||
-    image === 4 ||
-    image === 5 ||
-    image === 7 ||
-    image === 10 ||
-    image === 11
-  )
-    return true;
-  return false;
-}
-
-function displayLabyrinth(arrGame) {
+function displayGrid() {
   myGame.innerHTML = "";
-  let content = "<table>";
+  let content = "";
   for (let i = 0; i < arrGame.length; i++) {
     content += "<tr>";
     for (let j = 0; j < arrGame[i].length; j++) {
       content += "<td>";
-      content += `<img src='images/${arrGame[i][j].img}.png' />`;
-      if (i === totalLine - 1 && j === totalColumn - 1) {
-        let pandaX = sizeIMG / 4 + sizeIMG * i;
-        let pandaY = sizeIMG / 4 + sizeIMG * j;
-        content += `<img src='images/panda.png' style='width:${
-          sizeIMG / 2
-        }px;height:${
-          sizeIMG / 2
-        }px;position:absolute;left:${pandaY}px;top:${pandaX}px'/>`;
-      }
-      if (i === playerPosition[0] && j === playerPosition[1]) {
-        let bearX = 25 + sizeIMG * playerPosition[0];
-        let bearY = 25 + sizeIMG * playerPosition[1];
-        content += `<img src='images/bear.png' style='width:${
-          sizeIMG / 2
-        }px;height:${
-          sizeIMG / 2
-        }px;position:absolute;left:${bearY}px;top:${bearX}px'/>`;
-      }
+      content += `<img src="images/${arrGame[i][j]}.png" id="${i}-${j}" oncontextmenu='return false' />`;
       content += "</td>";
     }
     content += "</tr>";
   }
-  content += "</table>";
+
   myGame.innerHTML = content;
 }
 
-function getCell(i, j) {
-  return arrGame[i][j];
+function switchImage(line, column, add) {
+  if (add) {
+    if (arrGame[line][column] === 15) arrGame[line][column] = 0;
+    arrGame[line][column]++;
+  } else {
+    if (arrGame[line][column] === 0) arrGame[line][column] = 15;
+    arrGame[line][column]--;
+  }
+  displayGrid();
 }
 
-addEventListener("keyup", function (event) {
-  if (!isIAOn && !gameOver) {
-    let playerX = playerPosition[0];
-    let playerY = playerPosition[1];
-
-    if ((event.keyCode === 37 || event.keyCode === 81) && playerY > 0) {
-      //left
-      if (getCell(playerPosition[0], playerPosition[1]).left) {
-        playerY--;
-      }
+function createLabyrinth() {
+  const totalLine = parseInt(document.querySelector("#totalLine").value);
+  const totalColumn = parseInt(document.querySelector("#totalColumn").value);
+  let arr = [];
+  for (let i = 0; i < totalLine; i++) {
+    let line = [];
+    for (let j = 0; j < totalColumn; j++) {
+      line.push(0);
     }
-    if ((event.keyCode === 38 || event.keyCode === 90) && playerX > 0) {
-      // top
-      if (getCell(playerPosition[0], playerPosition[1]).top) {
-        playerX--;
-      }
-    }
-    if (
-      (event.keyCode === 39 || event.keyCode === 68) &&
-      playerY < totalColumn - 1
-    ) {
-      // right
-      if (getCell(playerPosition[0], playerPosition[1]).right) {
-        playerY++;
-      }
-    }
-    if (
-      (event.keyCode === 40 || event.keyCode === 83) &&
-      playerX < totalLine - 1
-    ) {
-      // bottom
-      if (getCell(playerPosition[0], playerPosition[1]).bottom) {
-        playerX++;
-      }
-    }
-    playerPosition = [playerX, playerY];
-    move();
+    arr.push(line);
   }
+  arrGame = arr;
+  displayGrid();
+}
+
+myGame.addEventListener("click", function () {
+  let id = event.target.id;
+  let dashID = id.indexOf("-");
+  let line = id.substring(0, dashID);
+  let column = id.substring(dashID + 1, id.length);
+  console.log(event.target);
+
+  switchImage(line, column, true);
 });
 
-function move() {
-  displayLabyrinth(arrGame);
-  checkEndOfLevel();
+myGame.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  let id = e.target.id;
+  let dashID = id.indexOf("-");
+  let line = id.substring(0, dashID);
+  let column = id.substring(dashID + 1, id.length);
+  console.log(e.target);
+
+  switchImage(line, column, false);
+});
+
+function save() {
+  let content = getContent();
+  let filename = "level.js";
+  let blob = new Blob([content], {
+    type: "text/plain;charset=utf-8",
+  });
+
+  saveAs(blob, filename);
 }
 
-function checkEndOfLevel() {
-  if (
-    playerPosition[0] === totalLine - 1 &&
-    playerPosition[1] === totalColumn - 1
-  ) {
-    let content = "";
-    if (currentLevel < levels.nbLevels) {
-      content += `<p>Bien joué ! Passer au niveau : ${currentLevel + 1}  ?</p>`;
-      content +=
-        "<button class='btn btn-primary' onClick='nextLevel()'> Suivant </button>";
-    } else {
-      content += "🎉 Vous avez gagné ! 🎉 ";
+function getContent() {
+  let content = "level : {";
+  content += "totalLine :" + tabJeu.length + ", ";
+  content += "totalColumn :" + tabJeu[0].length + ", ";
+  for (var i = 0; i < tabJeu.length; i++) {
+    content += "line" + (i + 1) + ": {";
+    for (var j = 0; j < tabJeu[i].length; j++) {
+      content += "case" + (j + 1) + ": " + tabJeu[i][j] + ", ";
     }
-
-    alert.innerHTML = content;
-    alert.classList.remove("d-none");
-    gameOver = true;
+    content += "},";
   }
-}
-
-function nextLevel() {
-  currentLevel++;
-  alert.classList.add("d-none");
-
-  totalLine = levels["level" + currentLevel].totalLine;
-  totalColumn = levels["level" + currentLevel].totalColumn;
-  playerPosition = [0, 0];
-  arrGame = loadLevel();
-
-  displayLabyrinth(arrGame);
-}
-
-function loadLevel() {
-  let arrLevel = [];
-  isIAOn = false;
-  gameOver = false;
-  IA.pastPosition = new Array();
-  IA.blockedPosition = new Array();
-  checkIA.removeAttribute("disabled");
-  checkIA.checked = false;
-
-  for (let i = 1; i <= levels["level" + currentLevel].totalLine; i++) {
-    let line = [];
-    for (let j = 1; j <= levels["level" + currentLevel].totalColumn; j++) {
-      let val = levels["level" + currentLevel]["line" + i]["case" + j];
-      line.push(createCell(val));
-    }
-    arrLevel.push(line);
-  }
-  return arrLevel;
+  content += "}";
+  return content;
 }
